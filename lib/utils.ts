52a672e2d14c528fcc87ebc7acdee7fce05f053f
sepidsoroush <s.soroush2012@gmail.com>
2024-03-13 11:44:35 +0200

    define Transaction types

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,23 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { BankTransaction, NormalizedTransaction } from "@/types";
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export function normalizeTransaction() {}
+
+export const normalizeUser = (transaction: BankTransaction | null) => {
+  return !transaction
+    ? null
+    : ({
+        accountNo: transaction["Customer account no"],
+        date: transaction.Date,
+        label: transaction["Sender/receiver name"],
+        amount: transaction.Amount,
+        description: transaction.Description,
+        category: transaction.Category,
+        incomeSource: transaction["Source of income"],
+      } satisfies NormalizedTransaction);
+};
--- types/index.d.ts
+++ types/index.d.ts
@@ -6,3 +6,35 @@ export interface NavItem {
   disabled?: boolean;
   icon?: LucideIcon;
 }
+
+export interface BankTransaction {
+  "Customer account no": string;
+  "Document no"?: string;
+  Date: string;
+  "Sender/receiver account": string;
+  "Sender/receiver name": string;
+  "Sender bank code": number;
+  "Debit/Credit (D/C)": string;
+  Amount: number;
+  "Reference number": string;
+  "Archiving code": string;
+  Description: string;
+  Fee: number;
+  Currency: string;
+  "Personal code or register code": number;
+  "Sender/receiver bank BIC": string;
+  "Transaction reference": number;
+  "Account servicer reference": string;
+  Category: string;
+  "Source of income": string;
+}
+
+export interface NormalizedTransaction {
+  accountNo: string;
+  date: string;
+  label?: string;
+  amount: number;
+  description?: string;
+  category: string;
+  incomeSource?: string;
+}
